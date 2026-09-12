598d781fc32a7f59c93f65d463973256f3415636
Ali Nafisi <alisafinal2001@gmail.com>
2026-09-12 16:21:50 +0330

    change name to GCIA

--- exps/__init__.py
+++ exps/__init__.py
@@ -2,5 +2,5 @@ from .cluster_based import ClusterBasedExp
 from .loss_based import LossBasedExp
 from .dfr import DFR
 from .entropy_based import EntropyBasedExp
-from .cia import CIA
+from .gcia import GCIA
 # from .gradcam_based import GradCAMExp
